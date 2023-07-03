42ca2d824dfc5b72c37e6089e2c91601a86cfb69
sh1ny08 <pardhushiny2008@gmail.com>
2023-07-03 18:14:09 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [role, setRole] = useState();
+    const showEmployees = true;
+    return ( <
+        div className = "App" > { " " } {
+            showEmployees ? ( <
+                >
+                <
+                input type = "text"
+                onChange = {
+                    {} => {
+                        console.log(e.target.value)
+                    }
+                }
+                /> <
+                Employee name = "Pardhu"
+                role = "intern" / >
+                <
+                Employee name = "Harsha" / >
+                <
+                Employee name = "Vivek" / >
+                <
+                />
+            ) : ( <
+                p > You cannot see the employees < /p>
+            )
+        } { " " } <
+        /div>
+    );
 }
 
 export default App;
